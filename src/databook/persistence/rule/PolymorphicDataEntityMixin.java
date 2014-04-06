5e7825b4501f1c72dff41cd98b7c7e2ebf25ddbe
Xu Hao <xuh@email.unc.edu>
2014-04-06 19:53:52 -0400

    clean up

--- src/databook/persistence/rule/PolymorphicDataEntityMixin.java
+++ src/databook/persistence/rule/PolymorphicDataEntityMixin.java
@@ -15,8 +15,6 @@ public interface PolymorphicDataEntityMixin {
 	@JsonSubTypes({  
 		    @Type(value = DataObject.class, name = "DataObject"),  
 		    @Type(value = Collection.class, name = "Collection"),
-		    @Type(value = Access.class, name = "Access"),
-		    @Type(value = Session.class, name = "Session"),
 		    @Type(value = Message.class, name = "Message"),
 		    @Type(value = Messages.class, name = "Messages"),
 		    @Type(value = User.class, name = "User"),
