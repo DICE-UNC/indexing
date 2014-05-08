ccd265ea48b369ea12898f431901336d48148a5d
Xu Hao <xuh@email.unc.edu>
2014-05-08 18:38:50 -0400

    make config class throw exceptions on missing property

--- src/databook/config/IrodsConfig.java
+++ src/databook/config/IrodsConfig.java
@@ -1,6 +1,5 @@
 package databook.config;
 
-import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class IrodsConfig {
@@ -13,17 +12,9 @@ public class IrodsConfig {
 	}
 
 	public static String getString(String key) {
-		try {
 			return RESOURCE_BUNDLE.getString(key);
-		} catch (MissingResourceException e) {
-			return '!' + key + '!';
-		}
 	}
 	public static int getInt(String key) {
-		try {
 			return Integer.parseInt(RESOURCE_BUNDLE.getString(key)) ;
-		} catch (MissingResourceException e) {
-			return -1;
-		}
 	}
 }
