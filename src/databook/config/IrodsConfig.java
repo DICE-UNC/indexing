48c30777fde95e2f6335f4b3fcdc36f58d73e6e1
Xu Hao <xuh@email.unc.edu>
2014-05-08 18:27:37 -0400

    add config directory

---
+++ src/databook/config/IrodsConfig.java
@@ -0,0 +1,22 @@
+package databook.config;
+
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+public class IrodsConfig {
+	private static final String BUNDLE_NAME = "databook.config.irods"; //$NON-NLS-1$
+
+	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
+			.getBundle(BUNDLE_NAME);
+
+	private IrodsConfig() {
+	}
+
+	public static String getString(String key) {
+		try {
+			return RESOURCE_BUNDLE.getString(key);
+		} catch (MissingResourceException e) {
+			return '!' + key + '!';
+		}
+	}
+}
